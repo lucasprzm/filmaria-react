2b4af46eea09fd258addded624baaf526bd7e7b5
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-02-23 09:25:29 -0300

    rotas favoritos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,12 @@
 import "./header.css";
 import { Link } from "react-router-dom";
-import Home from "../../pages/Home";
 export default function Header() {
   return (
     <header>
-      <Link className="logo" to={<Home />}>
+      <Link className="logo" to="/">
         Filmaria
       </Link>
-      <Link className="favoritos" to={<Home />}>
+      <Link className="favoritos" to="/favoritos">
         Salvos
       </Link>
     </header>
